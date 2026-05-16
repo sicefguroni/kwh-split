9da8579f740e9ac1632e5eb36b366bc3234241be
jpty016 <jpty016@gmail.com>
2026-05-16 14:12:06 +0800

    Fixing deployment

--- infra/terraform/cloudfront.tf
+++ infra/terraform/cloudfront.tf
@@ -34,7 +34,7 @@ resource "aws_cloudfront_distribution" "site" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "${local.name} SPA"
+  comment             = "${local.name} SPA - v2.1"
   default_root_object = "index.html"
   price_class         = var.cloudfront_price_class
   aliases             = local.cloudfront_aliases
@@ -121,7 +121,7 @@ resource "aws_cloudfront_distribution" "site" {
     content {
       acm_certificate_arn      = aws_acm_certificate.site[0].arn
       ssl_support_method       = "sni-only"
-      minimum_protocol_version = "TLSv1.2_2021"
+      minimum_protocol_version = "TLSv1.2_2019"
     }
   }
 
